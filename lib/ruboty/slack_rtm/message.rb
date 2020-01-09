c4eefc4c6ae2a9f7aad70c1bd052ba5b90b4ecc1
Akihiko Horiuchi <12ff5b8@gmail.com>
2020-01-09 23:55:49 +0900

    Always reply to thread if possible

--- lib/ruboty/slack_rtm/message.rb
+++ lib/ruboty/slack_rtm/message.rb
@@ -3,13 +3,22 @@ require 'ruboty/message'
 module Ruboty
   module SlackRTM
     module Message
+      def reply(body, options = {})
+        attributes = { body: body, from: to, to: from, original: original, thread_ts: thread_ts }.merge(options)
+        robot.say(attributes)
+      end
+
       def add_reaction(reaction)
         channel_id = @original[:from]
         timestamp  = sprintf("%.6f", @original[:time].to_f)
         robot.add_reaction(reaction, channel_id, timestamp)
       end
+
+      def thread_ts
+        @original[:thread_ts]
+      end
     end
   end
 
-  Message.include SlackRTM::Message
+  Message.prepend SlackRTM::Message
 end
